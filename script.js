a4e265c2dea81eb2b65322321b3a724b374a2c76
Koradiya Nirav <koradiyahari@gmail.com>
2023-08-15 14:08:53 +0530

    binance lightweight chart created

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ exampleSocket.onmessage = function (event) {
   var oriPrice = JSON.parse(event.data);
   newrPrice = parseFloat(oriPrice.p).toFixed(2);
 
-  tradeDiv.innerHTML = `<div class='text-[#848e9c]'>$</div>${newrPrice}`;
+  tradeDiv.innerHTML = `<div class='text-[#848e9c]'>$ </div>${newrPrice}`;
 
   if (newrPrice > currentPrice) {
     document.getElementById("treding").style.color = "#0ecb81";
+    tradeDiv.textContent += " \u2191";
   } else if (newrPrice < currentPrice) {
     document.getElementById("treding").style.color = "#f6465d";
+    tradeDiv.textContent += " \u2193";
   }
 
   currentPrice = newrPrice;
@@ -45,9 +47,9 @@ exampleSocket.onmessage = function (event) {
       const time = document.createElement("td");
 
       if (traders[i][3]) {
-        price.style.color = "#0ecb81";
-      } else {
         price.style.color = "#f6465d";
+      } else {
+        price.style.color = "#0ecb81";
       }
 
       price.textContent = traders[i][0];
@@ -133,4 +135,128 @@ btcSocket.onmessage = function (event) {
     row1.appendChild(amountm);
     bitBody.appendChild(row1);
   }
+  const tableBody = document.getElementById("bitBody");
+  const rows = Array.from(tableBody.getElementsByTagName("tr"));
+
+  rows.reverse();
+
+  tableBody.innerHTML = "";
+
+  for (const row of rows) {
+    tableBody.appendChild(row);
+  }
+};
+
+const chartContainer1 = document.getElementById("chart");
+const chart0 = LightweightCharts.createChart(chartContainer1, {
+  layout: {
+    background: {
+      type: "solid",
+      color: "transparent",
+    },
+    textColor: "rgba(255, 255, 255, 0.9)",
+  },
+  grid: {
+    vertLines: {
+      color: "rgba(197, 203, 206, 0.5)",
+    },
+    horzLines: {
+      color: "rgba(197, 203, 206, 0.5)",
+    },
+  },
+  crosshair: {
+    mode: LightweightCharts.CrosshairMode.Noramal,
+  },
+  rightPriceScale: {
+    borderColor: "rgba(197, 203, 206, 0.8)",
+  },
+  timeScale: {
+    borderColor: "rgba(197, 203, 206, 0.8)",
+  },
+});
+
+var candleSeries = chart0.addCandlestickSeries({
+  upColor: "#0ecb81",
+  downColor: "#f6465d",
+  borderDownColor: "rgba(255, 144, 0, 1)",
+  borderUpColor: "rgba(255, 144, 0, 1)",
+  wickDownColor: "rgba(255, 144, 0, 1)",
+  wickUpColor: "rgba(255, 144, 0, 1)",
+});
+
+fetch(`https://fapi.binance.com/fapi/v1/klines?symbol=btcusdt&interval=1d`)
+  .then((response) => response.json())
+
+  .then((data) => {
+    const ohlc = data.map((item) => ({
+      time: item[0] / 1000,
+      open: parseFloat(item[1]),
+      high: parseFloat(item[2]),
+      low: parseFloat(item[3]),
+      close: parseFloat(item[4]),
+    }));
+
+    const candleSeries = chart0.addCandlestickSeries();
+    candleSeries.setData(ohlc);
+  });
+
+const { createChart } = LightweightCharts;
+
+const chartContainer = document.getElementById("chart");
+
+const chart = createChart(chartContainer, {
+  layout: {
+    background: {
+      type: "solid",
+      color: "#000000",
+    },
+    textColor: "rgba(255, 255, 255, 0.9)",
+  },
+  grid: {
+    vertLines: {
+      color: "rgba(197, 203, 206, 0.5)",
+    },
+    horzLines: {
+      color: "rgba(197, 203, 206, 0.5)",
+    },
+  },
+  crosshair: {
+    mode: LightweightCharts.CrosshairMode.Normal,
+  },
+  rightPriceScale: {
+    borderColor: "rgba(197, 203, 206, 0.8)",
+  },
+  timeScale: {
+    borderColor: "rgba(197, 203, 206, 0.8)",
+  },
+});
+
+var candleSeries = chart.addCandlestickSeries({
+  upColor: "rgba(255, 144, 0, 1)",
+  downColor: "#000",
+  borderDownColor: "rgba(255, 144, 0, 1)",
+  borderUpColor: "rgba(255, 144, 0, 1)",
+  wickDownColor: "rgba(255, 144, 0, 1)",
+  wickUpColor: "rgba(255, 144, 0, 1)",
+});
+
+const candlestickSeries = chart.addCandlestickSeries();
+
+const binanceWebSocketUrl = "wss://stream.binance.com:9443/ws/btcusdt@kline_1m";
+
+const ws = new WebSocket(binanceWebSocketUrl);
+
+ws.onmessage = function (event) {
+  var data = JSON.parse(event.data);
+
+  const klineData = data.k;
+
+  candlestickSeries.update({
+    time: klineData.t / 1000,
+    open: parseFloat(klineData.o),
+    high: parseFloat(klineData.h),
+    low: parseFloat(klineData.l),
+    close: parseFloat(klineData.c),
+    symbol: klineData.s,
+  });
 };
